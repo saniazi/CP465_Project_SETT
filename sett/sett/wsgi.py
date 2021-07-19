0c2d56c0dd90c17507f218040ada2df1ae82d698
saniazi <59815152+saniazi@users.noreply.github.com>
2021-07-19 07:53:37 -0400

    Changed settings path

--- sett/sett/wsgi.py
+++ sett/sett/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sett.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sett.sett.settings')
 
 application = get_wsgi_application()
