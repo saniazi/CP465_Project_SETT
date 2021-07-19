44af314223c3ce313d98c59cbfb627381311b884
saniazi <59815152+saniazi@users.noreply.github.com>
2021-07-19 17:58:45 -0400

    debug

--- sett/sett/settings.py
+++ sett/sett/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = CURR_DIR.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'saniazi-sett.herokuapp.com']
 
